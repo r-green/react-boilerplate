20052210946b1dff5660ced79251026ab7b9190f
Richard Green <rg@ie.com.au>
2016-06-09 15:01:09 +1000

    added localstorage and refactored app into root component

--- src/localStorage.js
+++ src/localStorage.js
@@ -15,7 +15,7 @@ export const loadState = () => {
 export const saveState = (state) => {
     try {
         const serializedState = JSON.stringify(state);
-        
+
         localStorage.setItem('state', serializedState)
     }  catch (err) {
         // ignore write errors
