350c1c0642c8d94f9cc2d137f503d8a06f22c6a0
Richard Green <rg@ie.com.au>
2016-06-09 14:25:35 +1000

    Added webpack config and dependencies (react, redux, babel etc) + localstorage

---
+++ src/components/FilterLink.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { setVisibilityFilter } from '../actions';
+import Link from './Link';
+
+
+const mapStateToProps = (state, ownPops) => ({
+    active: ownPops.filter === state.visibilityFilter
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    onClick() {
+        dispatch(setVisibilityFilter(ownProps.filter));
+    }
+});
+
+const FilterLink = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Link);
+
+export default FilterLink;
